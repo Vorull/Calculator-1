aacd08614a8b76d341b8fc541786152db9794f90
KopytovSV <bekflip2013@ya.ru>
2018-04-19 00:50:51 +0500

    Конструкторы: чекни

--- Data.cs/Data.cs/Log.cs
+++ Data.cs/Data.cs/Log.cs
@@ -11,27 +11,22 @@ namespace Data.cs
         /// <summary>
         /// основание логорифма
         /// </summary>
-        double a;
+        double a=1;
         /// <summary>
         /// подлогорифмическое выражение
         /// </summary>
-        double b;
+        double b=1;
         /// <summary>
         /// значение log
         /// </summary>
-        double value;
+        double value=1;
         /// <summary>
         /// если true, то заполнен верно 
-        /// </summary>
-        bool flag;
-        //Свойства 
-        /// <summary>
-        /// Получение/заполение основания
         /// </summary>
         public double A
         {
             get { return a; }
-            set { if (value > 0) a = value; }
+            set { if (value > 0) a = value; else a = 1; }
         }
         /// <summary>
         /// Получение/заполение под. лог. выраж.
@@ -39,18 +34,38 @@ namespace Data.cs
         public double B
         {
             get { return b; }
-            set { if (value > 0) b = value; }
+            set { if (value > 0) b = value; else  b = 1;  }
         }
+        /// <summary>
+        /// Получить значение log
+        /// (присваивать значения нельзя/только получить)</summary>
+        public double Value
+        {
+            get { double buf;buf = Math.Log(a)/Math.Log(b); return buf; }
+        }
+       
         /// <summary>
         /// Конструктор без параметров
         /// </summary>
         public Log()
         {
-
+            this.A = 1;
+            this.B = 1;
         }
-    
+        /// <summary>
+        /// Конструктор с параметрами
+        /// </summary>
+        /// <param name="A">основание</param>
+        /// <param name="B">под. лог. выражение</param>
+        public Log(double A, double B)
+        {
+            this.A = A;
+            this.B = B;
+        }
+       
 
     }
+    
 
 
     
