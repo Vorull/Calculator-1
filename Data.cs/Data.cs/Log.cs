44188be5d0c972a4d7f421b3d8f857e49e15e46f
KopytovSV <bekflip2013@ya.ru>
2018-04-19 00:18:37 +0500

    Конструкторы:Начало

--- Data.cs/Data.cs/Log.cs
+++ Data.cs/Data.cs/Log.cs
@@ -8,22 +8,52 @@ namespace Data.cs
 {
     class Log
     {
+        /// <summary>
+        /// основание логорифма
+        /// </summary>
         double a;
+        /// <summary>
+        /// подлогорифмическое выражение
+        /// </summary>
         double b;
+        /// <summary>
+        /// значение log
+        /// </summary>
         double value;
+        /// <summary>
+        /// если true, то заполнен верно 
+        /// </summary>
         bool flag;
-
         //Свойства 
+        /// <summary>
+        /// Получение/заполение основания
+        /// </summary>
+        public double A
+        {
+            get { return a; }
+            set { if (value > 0) a = value; }
+        }
+        /// <summary>
+        /// Получение/заполение под. лог. выраж.
+        /// </summary>
+        public double B
+        {
+            get { return b; }
+            set { if (value > 0) b = value; }
+        }
+        /// <summary>
+        /// Конструктор без параметров
+        /// </summary>
+        public Log()
+        {
+
+        }
+    
 
     }
 
-   
 
-
-
-
-
-
-    }
+    
+}
 
 
